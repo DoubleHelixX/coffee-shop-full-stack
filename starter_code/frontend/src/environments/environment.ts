d9738baf78c8d45ab09497a9d67e56619e0edfc4
DoubleHelix <tjrnyc5@gmail.com>
2020-07-14 13:32:36 -0400

    Configed the auth0 api setting enabling connectivity to auth0 api client

--- starter_code/frontend/src/environments/environment.ts
+++ starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,10 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'double-helixx.us', // the auth0 domain prefix
+    audience: 'image', // the audience set for the auth0 app
+    clientId: 'l6yng5LFtKZIFZ6I56fgPyJqbfJ3yg8U', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
+///tabs/user-page
